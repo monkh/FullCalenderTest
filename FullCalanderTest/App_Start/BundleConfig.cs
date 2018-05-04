bfc72aa98b1390632c4a6896b8cd83228a50e587
Luke Varley <luke.varley@auger.co.uk>
2018-05-05 00:03:42 +0100

    Inital commit and basic calendar working.

--- FullCalanderTest/App_Start/BundleConfig.cs
+++ FullCalanderTest/App_Start/BundleConfig.cs
@@ -8,8 +8,8 @@ namespace FullCalanderTest
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,9 +23,22 @@ namespace FullCalanderTest
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Styles/css").Include(
+                      "~/Styles/bootstrap.css",
+                      "~/Styles/site.css"));
+
+            //scripts
+            bundles.Add(new ScriptBundle("~/bundles/calendarscripts").Include(
+                                        "~/Scripts/jquery-{version}.js",
+                                        "~/Scripts/Calendar/moment.min.js",
+                                        "~/Scripts/Calendar/fullcalendar.min.js",
+                                        "~/Scripts/Calendar/gcal.min.js",
+                                        "~/Scripts/Calendar/jquery-ui.min.js"));
+            //styles
+            bundles.Add(new StyleBundle("~/bundles/calendarstyles").Include(
+                                        "~/Styles/Calendar/fullcalendar.min.css",
+                                        "~/Styles/Calendar/scheduler.min.css",
+                                        "~/Styles/Calendar/CustomCalStyles.css"));
         }
     }
 }
